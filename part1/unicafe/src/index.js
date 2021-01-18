b64091d5d8cb1f5642f755e18d5da3dae73ae7f5
Dan Wilder <danwilder@Dans-MacBook-Pro.local>
2021-01-18 15:26:14 -0600

    Complete 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,13 +1,38 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Statistic = ({ text, count }) => (
+  <p>
+    {text} {count}
+  </p>
+);
+
+const Feedback = () => <h1>give feedback</h1>;
+
+const Statistics = () => <h1>statistics</h1>;
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <Feedback />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics />
+      <Statistic text="good" count={good} />
+      <Statistic text="neutral" count={neutral} />
+      <Statistic text="bad" count={bad} />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
