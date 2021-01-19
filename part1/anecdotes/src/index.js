93f6ab6ec3d8743a074705b1fdfc600427fc03c3
Dan Wilder <danwilder@Dans-MacBook-Pro.local>
2021-01-19 09:38:21 -0600

    Complete 1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -5,6 +5,12 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
+const Anecdote = ({ anecdote, votes }) => (
+  <p>
+    {anecdote} <br /> has {votes} votes
+  </p>
+);
+
 const App = (props) => {
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0));
@@ -17,17 +23,25 @@ const App = (props) => {
   };
 
   const handleVoteClick = () => {
-    const copy = { ...votes };
+    const copy = [...votes];
     copy[selected]++;
     setVotes(copy);
   };
 
+  const indexMax = votes.reduce(
+    (iMax, x, i, arr) => (x > arr[iMax] ? i : iMax),
+    0
+  );
+
   return (
     <div>
-      <p>{props.anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
+      <h1>Anecdote of the day</h1>
+      <Anecdote anecdote={props.anecdotes[selected]} votes={votes[selected]} />
       <Button handleClick={handleVoteClick} text="vote" />
       <Button handleClick={handleAnecdoteClick} text="next anecdote" />
+
+      <h1>Anecdote with most votes</h1>
+      <Anecdote anecdote={props.anecdotes[indexMax]} votes={votes[indexMax]} />
     </div>
   );
 };
