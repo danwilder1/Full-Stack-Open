45394387e4c97881e88d420b6930449a8607ad17
Dan Wilder <danwilder@Dans-MacBook-Pro.local>
2021-01-19 17:59:16 -0600

    Complete 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,21 @@ import Numbers from "./components/Numbers";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+
+  const personsToShow =
+    newFilter === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toUpperCase().includes(newFilter.toUpperCase())
+        );
 
   const addName = (e) => {
     e.preventDefault();
@@ -31,10 +42,19 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const handleFilterChange = (e) => {
+    setNewFilter(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={newFilter} onChange={handleFilterChange} />
+      </div>
 
+      <h3>add a new</h3>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -47,7 +67,7 @@ const App = () => {
         </div>
       </form>
 
-      <Numbers persons={persons} />
+      <Numbers persons={personsToShow} />
     </div>
   );
 };
